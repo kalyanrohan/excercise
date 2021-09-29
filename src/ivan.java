027c16f6ecccec16ea9b021acbab4e79e6a0c0bd
kalyanrohan <sri.rohan@binus.ac.id>
2021-09-29 18:29:24 +0700

    balalbabalba

---
+++ src/ivan.java
@@ -0,0 +1,35 @@
+public class ivan {
+    private String sex;
+    private String firstName;
+    private int age;
+
+    public ivan(String sex, String firstName, int age) {
+        this.sex = sex;
+        this.firstName = firstName;
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
